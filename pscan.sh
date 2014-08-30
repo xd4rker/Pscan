6ac2b8008bc53456bf0ee9c25da7fe1fbc91e7d7
Ismail <xd4rker@gmail.com>
2014-08-30 20:28:15 +0100

    Update pscan.sh

--- pscan.sh
+++ pscan.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 clear
+echo " ____"
+echo "|  _ \ ___  ___ __ _ _ __"
+echo "| |_) / __|/ __/ _\` | '_ \ "
+echo "|  __/\__ \ (_| (_| | | | |"
+echo "|_|   |___/\___\__,_|_| |_|"
+echo "                Version 0.1"
 echo -e "\nPlease select your option :"
 echo -e "\n1. Internal scan"
 echo -e "2. External scan"
@@ -8,28 +14,26 @@ echo -e "3. Quit"
 echo -ne "\nOption > "
 read option
 
+
 case $option in
 	"1")	ip_add=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -v "127.0.0.1" | cut -d: -f2`;;
 	"2")	ip_add=`curl ipecho.net/plain --silent`;;
 	"3")	exit;;
-	*)		ip_add="ERROR";;
+	*)		ip_add="ERROR"; exit;;
 esac
 
 clear
 echo -ne "\n[+] Fetching your IP address : "
+
 sleep 1
 echo $ip_add
 net_ip=`echo $ip_add | cut -d. -f1,2,3`
 sleep 1
 echo -e "\n[+] Scanning the network\n"
-if [[ $# -eq 1 ]]
-then
-	proxy="proxychains"
-fi
 
 for i in {1..254}
 do
-	$proxy ping -w 2 $net_ip.$i &> /dev/null
+	ping -w 2 $net_ip.$i &> /dev/null
 	if [ $? -eq 0 ]
 	then
 		echo "$ip_add.$i : Host is up"
